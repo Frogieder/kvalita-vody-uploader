87e27dc0aae7d7f9757a372a39beb6ec5262d28a
Martin <frogieder@gmail.com>
2022-05-13 06:29:24 +0200

    Implement uploading method

--- src/uploader.py
+++ src/uploader.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+
 class Uploader:
     def __init__(self, url, token, organization, bucket):
         self.bucket = bucket
@@ -5,5 +10,19 @@ class Uploader:
         self.token = token
         self.url = url
 
-    def upload(self, data):
-        print(data)
+    def upload(self, data: dict):
+        # print(data)
+        sequence = [
+            Point("kvalita-vody")
+                .tag("location", "Šrobárka")
+                .field(key, data[key])
+                .time(datetime.utcnow(), WritePrecision.NS)
+            for key in data.keys()
+        ]
+
+        with InfluxDBClient(url="self.url", token=self.token, org=self.organization) as client:
+            write_api = client.write_api(write_options=SYNCHRONOUS)
+            write_api.write(self.bucket, self.organization, sequence)
+            print("Uploading to database")
+            print(sequence)
+
